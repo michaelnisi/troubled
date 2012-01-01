9325241390edff49b75551bc601b41dd085467f9
Michael Nisi <michael.nisi@gmail.com>
2012-01-01 22:15:00 +0100

    Improve typography

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,27 +2,13 @@
  * script.js
  *
  * @author Michael Nisi (mn@michaelnisi.com)
- */
+*/
 
 $(document).ready(function() { 
 	$('#likes').load('/likes.html');
 	$('#tweet').load('/tweet.html');
+	$('pre').next('p').css({ 'color':'#ccc', 'margin-top':'-.3em', 'font-size':'1.3em' });
 	$('pre code').each(function(i, e) {
-		hljs.highlightBlock(e, '    ')
+		hljs.highlightBlock(e, '    ');	
 	});
-
-	var articles = $('#articles');
-	
-	if (articles && $(window).width() > 768) {
-		articles.append('<ul id="secondArticlesColumn"></ul>');
-
-		var items =  $('#articles ul li');
-		var threshold = items.length / 2;
-				
-		items.each(function(index) {
-			if (index >= threshold) {
-				$(this).appendTo($('#secondArticlesColumn'));
-			}
-		});
-	}
 });
