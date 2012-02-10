be8c6473d8500c9f9b4701a7bda59497f3c46911
Michael Nisi <michael.nisi@gmail.com>
2012-02-10 18:57:49 +0100

    Add views

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,23 +1,25 @@
-/**
- * script.js
- *
- * @author Michael Nisi (mn@michaelnisi.com)
-*/
+// script.js 
 
-$(document).ready(function() { 
+var troubled = troubled || {};
+
+troubled.moveFooter = function () {
+	// TODO: Remove concrete pixel value
+	$('#container').css({ 'min-height':$(window).height() - 32 - $('#page_footer').height() });
+}
+
+$(document).ready(function () { 
 	$('#likes').load('/likes.html');
 	$('#tweet').load('/tweet.html');
-	
+
 	$('pre').next('p').css({ 'color':'#ccc', 'margin-top':'-.3em', 'font-size':'1.3em' });
-	
-	$('pre code').each(function(i, e) {
+
+	$('pre code').each(function (i, e) {
 		hljs.highlightBlock(e, '    ');	
 	});
-	
-	if ($('#container').height() + $('#page_footer').height() + 200 < $(window).height()) {
-		$('#page_footer').css({ 'position':'fixed', 'left':'0', 'bottom':'0', 'height':'inherit' });
-	} else {
-		$('#page_footer').css({ 'position':'relative', 'height':'100%' });
-	}
+
+	troubled.moveFooter();
 });
 
+$(window).resize(function () {
+	troubled.moveFooter();
+});
---
+++ views/home.js
@@ -0,0 +1,71 @@
+// The home.js module renders the home page.
+
+var step = require('step');
+var jade = require('jade');
+var blake = require('blake');
+var articles = require('./article.js');
+
+// Add item to specified items array.
+var addItem = function(name, items, paths, callback) {
+  blake.readFile(name, function(err, file) {
+    if (err) {
+      throw err;
+    }
+    var src, item;
+
+    src = blake.getSource(file, name, paths);
+    item = articles.getJadeLocals(src);
+    
+    items.push(item);
+    callback(err, items);
+  });
+};
+
+// Bake the home page.
+var bake = function(src, callback) {
+  var items, result, options, jadeCompile;
+
+  items = [];
+  options = { filename: src.templatePath, pretty: true };
+  jadeCompile = jade.compile(src.template, options);
+
+  step(
+    function() {
+      blake.readDir(src.paths.posts, this);
+    },
+    function(err, names) {
+      if (err) {
+        throw err;
+      }
+
+      var group = this.group();
+      names.forEach(function(name) {
+        addItem(name, items, src.paths, group());
+      });
+    },
+    function(err, files) {
+      if (err) {
+        throw err;
+      }
+
+      items.sort(function(a, b) { // descending by time
+        return (a.time - b.time) * -1;
+      });
+
+      var latestItem = items[0];
+
+      result = jadeCompile({
+        mainNavigationItems: src.header.menu,
+        title: src.header.title,
+        items: items,
+        dateString: latestItem ? latestItem.dateString : src.dateString
+      });
+
+      callback(null, src.path, src.name, result);
+    }
+  );
+};
+
+module.exports = {
+  bake:bake
+};
