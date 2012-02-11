8d1e3ce337d2cc8b6c1ff976db949608fe72a4e2
Michael Nisi <michael.nisi@gmail.com>
2012-02-11 16:39:33 +0100

    Write views in CoffeeScript

---
+++ coffee/about.coffee
@@ -0,0 +1,28 @@
+# This module bakes the about page.
+
+jade = require 'jade'
+markdown = (require 'markdown').markdown
+
+# Create jade options with filename set to template, which is required for 
+# template includes and template inheritance. Setup Jade compile function with
+# the template and the options. And finally compile jade template with
+# according locals.
+bake = (src, callback) ->
+	options = 
+		filename: src.templatePath
+		pretty: true
+	
+	jadeCompile = jade.compile src.template, options
+	
+	result = jadeCompile
+		mainNavigationItems: src.header.menu
+		title: src.header.title
+		description: src.header.description
+		content: markdown.toHTML src.body
+		dateString: src.dateString
+
+	callback null, src.path, src.name, result
+
+# Export API.
+module.exports = 
+	bake: bake
--- views/about.js
+++ views/about.js
@@ -1,26 +1,29 @@
-// This module bakes the about page.
+(function() {
+  var bake, jade, markdown;
 
-var jade = require('jade');
-var markdown = require('markdown').markdown;
+  jade = require('jade');
 
-// Create jade options with filename set to template, which is required for 
-// template includes and template inheritance. Setup Jade compile function with
-// the template and the options. And finally compile jade template with
-// according locals.
-var bake = function(src, callback) {
-  var options = { filename: src.templatePath, pretty: true };
-  var jadeCompile = jade.compile(src.template, options);
-  var result = jadeCompile({
-    mainNavigationItems: src.header.menu,
-    title: src.header.title,
-    description: src.header.description,
-    content: markdown.toHTML(src.body),
-    dateString: src.dateString
-  });
+  markdown = (require('markdown')).markdown;
 
-  callback(null, src.path, src.name, result);
-};
+  bake = function(src, callback) {
+    var jadeCompile, options, result;
+    options = {
+      filename: src.templatePath,
+      pretty: true
+    };
+    jadeCompile = jade.compile(src.template, options);
+    result = jadeCompile({
+      mainNavigationItems: src.header.menu,
+      title: src.header.title,
+      description: src.header.description,
+      content: markdown.toHTML(src.body),
+      dateString: src.dateString
+    });
+    return callback(null, src.path, src.name, result);
+  };
 
-module.exports = {
-  bake:bake
-};
+  module.exports = {
+    bake: bake
+  };
+
+}).call(this);
